8967edc1cde98990cf215cf2f3ee9a99a56e7abe
mael <mael@morin.fr>
2024-03-25 16:24:02 +0100

    V2.0

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,16 @@ public class Main {
         //System.out.println(T1.toString());
         The T2 = new The("Thé 2", Categorie.Noir,"Inde",95,6.5F);
         //System.out.println(T2.toString());
+        The T3 = new The("Thé 3", Categorie.Noir,"Inde",95,9F);
+        The T4 = new The("Thé 4", Categorie.Noir,"Inde",95,9.5F);
+
         ST1.ajouter(T1,50);
         ST1.ajouter(T2,60);
+        ST1.ajouter(T3,50);
+        ST1.ajouter(T4,60);
+
+        ST1.thesDeCategorie(Categorie.Noir);
+        System.out.println(ST1.quantiteDeThesDontLaNoteEstSupOuEgaleA(8));
         ST1.afficherStock();
         ST1.sortir(T1,40);
         ST1.sortir(T2,60);
